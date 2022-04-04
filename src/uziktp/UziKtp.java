ebd2f5c3330258017ef07a209ab61b9062f5d778
FahmiFauzi26 <fahmifauziabdullah26@gmail.com>
2022-04-04 10:50:00 +0700

    Upload Folder

---
+++ src/uziktp/UziKtp.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uziktp;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author uzi
+ */
+public class UziKtp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        int x,y;
+        for (x = 0; x <=5; x++) {
+           //perulangan ke bawah
+            System.out.println();
+            //Enter
+  
+            for ( y = 0; y < x; y++) {
+                //perulangan ke samping
+                System.out.print(" * ");
+                //menampilkan bintang pada output  
+                
+            }
+        }
+
+        //deklarasi dinamis
+        // Scanner b = new Scanner(System.in);
+        // String nik, nama, ttl, jk, alamat, rtrw, desa, kecamatan, agama, statusPkw, pekerjaan, kewar, berlk,goldar;
+
+
+        // //deklarasi 
+        // String header = "PROVINSI JAWA TIMUR";
+        // String header2 = "KOTA SURABAYA";
+       ///////////////////////////////////////deklarasi statis
+    //    String nik = "3212039485767773";
+
+    //    String nama = "Fahmi Fauzi Abdullah";
+    //    String ttl = "Gresik, 03-03-1993";
+    //    String jk = "laki - laki";
+    //    char goldar = 'B';
+    //    String alamat = "Jalan lidah buaya 99 ";
+    //    String rtrw = "001/002";
+    //    String desa = "Sukakamu";
+    //    String kecamatan = "sukolilo";
+    //    String agama ="ISLAM";
+    //    String statusPkw ="Belum Menikah";
+    //    String pekerjaan = "Wiraswasta";
+    //    String kewar= "WNI";
+    //    String berlk= "SEUMUR HIDUP";
+       
+    //     /////////////////////////// memanggilStatis
+    //    System.out.println("                            " + header);
+    //    System.out.println("                                " + header2);
+    //    System.out.println("                              "+nik);
+    //    System.out.println("Nama                    : " + nama);
+    //    System.out.println("Tempat/tanggal lahir    : " + ttl);
+    //    System.out.println("Jenis kelamin           : " + jk + "        Gol.Darah : " + goldar);
+    //    System.out.println("Alamat                  : " + alamat);
+    //    System.out.println("    RT/Rw               : " + rtrw);
+    //    System.out.println("    Kel/Desa            : " + desa);
+    //    System.out.println("    Kecamatan           : " + kecamatan);
+    //    System.out.println("Agama                   : " + agama);
+    //    System.out.println("Status Perkawinan       : " + statusPkw);
+    //    System.out.println("Pekerjaan               : " + pekerjaan);
+    //    System.out.println("Kewarganegaraan         : " + kewar + "                                                SURABAYA");
+    //    System.out.println("Berlaku Hingga          : " + berlk + "                                      20-09-2021");
+        ////memanggil dinamis
+        // System.out.print("Masukkan nik            : ");
+        // nik = b.nextLine();
+        // System.out.print("Masukkan nama           : ");
+        // nama = b.nextLine();
+        // System.out.print("Masukkan ttl            : ");
+        // ttl = b.nextLine();
+        // System.out.print("Masukkan jenis kelamin  : ");
+        // jk = b.nextLine();
+        // System.out.print("Masukkan golongan darah : ");
+        // goldar = b.nextLine();
+        // System.out.print("Masukkan Alamat         : ");
+        // alamat = b.nextLine();
+        // System.out.print("Masukkan RT/RW          : ");
+        // rtrw = b.nextLine();
+        // System.out.print("Masukkan Kel/Desa       : ");
+        // desa = b.nextLine();
+        // System.out.print("Masukkan kecamatan      : ");
+        // kecamatan = b.nextLine();
+        // System.out.print("Masukkan Agama          : ");
+        // agama = b.nextLine();
+        // System.out.print("Masukkan Status         : ");
+        // statusPkw = b.nextLine();
+        // System.out.print("Masukkan Pekerjaan      : ");
+        // pekerjaan = b.nextLine();
+        // System.out.print("Masukkan Kewarganegaraan: ");
+        // kewar = b.nextLine();
+        // System.out.print("Masukkan Masa Berlaku   : ");
+        // berlk = b.nextLine();
+        
+        // //////memanggil
+        // System.out.println("===========================================");
+        //         System.out.println("                            " + header);
+        // System.out.println("                                " + header2);
+        // System.out.println("Nama                    : " + nama);
+        // System.out.println("Tempat/tanggal lahir    : " + ttl);
+        // System.out.println("Jenis kelamin           : " + jk + "        Gol.Darah : " + goldar);
+        // System.out.println("Alamat                  : " + alamat);
+        // System.out.println("    RT/Rw               : " + rtrw);
+        // System.out.println("    Kel/Desa            : " + desa);
+        // System.out.println("    Kecamatan           : " + kecamatan);
+        // System.out.println("Agama                   : " + agama);
+        // System.out.println("Status Perkawinan       : " + statusPkw);
+        // System.out.println("Pekerjaan               : " + pekerjaan);
+        // System.out.println("Kewarganegaraan         : " + kewar + "                                                SURABAYA");
+        // System.out.println("Berlaku Hingga          : " + berlk + "                                      20-09-2021");
+       
+// if(agama=="ISLAM"){
+//             System.out.println("Assalamualaikum");
+// }
+// else if (agama=="HINDU"){
+//     System.out.println("Om Swastyastu");
+// }
+// else{
+//     System.out.println("Terimakasih");
+//     }
+    }
+
+}
